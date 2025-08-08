afd794952bc54ca8805def63d931cbedf03bd228
Teteiba-Ange <aacquah041@st.ug.edu.gh>
2025-08-08 15:39:45 +0100

    changed

--- dcit318-assignment3-11033579.cs/Program.cs
+++ dcit318-assignment3-11033579.cs/Program.cs
@@ -1,61 +1,96 @@
-﻿using System.Transactions;
+﻿using System;
 
-public class Record { 
+public record Transaction
+(
+    int Id,
+    DateTime Date,
+    decimal Amount,
+    string Category
+);
 
-    public class Transaction()
-    {
-        int Id;
-        DateTime Date;
-        Decimal Amount;
-        String Category;
-    }
-}
-
-public interface ItransactionProcessor
+// Interface
+public interface ITransactionProcessor
 {
     void Process(Transaction transaction);
-
 }
 
-publc class BankTransferProcessor : ItransactionProcessor
+// Bank Transfer Processor
+public class BankTransferProcessor : ITransactionProcessor
 {
-    void Process()
+    public void Process(Transaction transaction)
     {
-       
         Console.WriteLine("This is your bank transfer");
-
-        Console.WriteLine("Please Enter Amount");
-        int user = Console.ReadLine();
-        Amount = user;
-        Console.WriteLine($"This is the Amount you entered {Amount}");
-        Console.WriteLine("Please Enter Category");
-        string user2 =Console.ReadLine();
-        Category = user2;
-        Console.WriteLine($"This is the category you entered {Category}");
+        Console.WriteLine($"Amount: {transaction.Amount}");
+        Console.WriteLine($"Category: {transaction.Category}");
+        
     }
 }
 
-
-publc class MobileMoneyProcessor : ItransactionProcessor
+// Mobile Money Processor
+public class MobileMoneyProcessor : ITransactionProcessor
 {
-    void Process()
+    public void Process(Transaction transaction)
     {
         Console.WriteLine("This is your Mobile Money transfer");
-        Console.WriteLine("Please Enter Amount");
-        int user3 = Console.ReadLine();
-        Amount = user3;
-        Console.WriteLine($"This is the Amount you entered {Amount}");
-        Console.WriteLine("Please Enter Category");
-        string user5 = Console.ReadLine();
-        Category = user5;
-        Console.WriteLine($"This is the category you entered{Category}");
+        Console.WriteLine($"Amount: {transaction.Amount}");
+        Console.WriteLine($"Category: {transaction.Category}");
+        Console.WriteLine($"Date: {transaction.Date}");
     }
 }
 
-publc class CryptoWalletProcessor : ItransactionProcessor
+// Crypto Wallet Processor
+public class CryptoWalletProcessor : ITransactionProcessor
 {
-    void Process()
+    public void Process(Transaction transaction)
     {
         Console.WriteLine("This is your Crypto wallet transfer");
+        Console.WriteLine($"Amount: {transaction.Amount}");
+        Console.WriteLine($"Category: {transaction.Category}");
+        
+    }
+}
+
+// Main program
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Amount:");
+        string inputAmount = Console.ReadLine();
+        decimal amount = decimal.Parse(inputAmount);
+
+        Console.WriteLine("Enter Category:");
+        string category = Console.ReadLine();
+
+        var transaction = new Transaction(
+            Id: 1,
+            Date: DateTime.Now,
+            Amount: amount,
+            Category: category
+        );
+
+        // Choose processor
+        ITransactionProcessor processor;
+
+        Console.WriteLine("Select processor: 1. Bank  2. Mobile  3. Crypto");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                processor = new BankTransferProcessor();
+                break;
+            case "2":
+                processor = new MobileMoneyProcessor();
+                break;
+            case "3":
+                processor = new CryptoWalletProcessor();
+                break;
+            default:
+                Console.WriteLine("Invalid choice.");
+                return;
+        }
+
+        processor.Process(transaction);
     }
 }
