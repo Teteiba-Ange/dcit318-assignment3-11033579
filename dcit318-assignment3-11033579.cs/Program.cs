0dd543aca81289531dc8658b5b9dc69283848dbf
Teteiba-Ange <aacquah041@st.ug.edu.gh>
2025-08-08 15:27:16 +0100

    updated

--- dcit318-assignment3-11033579.cs/Program.cs
+++ dcit318-assignment3-11033579.cs/Program.cs
@@ -26,8 +26,12 @@ publc class BankTransferProcessor : ItransactionProcessor
 
         Console.WriteLine("Please Enter Amount");
         int user = Console.ReadLine();
-        Amount = user
+        Amount = user;
+        Console.WriteLine($"This is the Amount you entered {Amount}");
         Console.WriteLine("Please Enter Category");
+        string user2 =Console.ReadLine();
+        Category = user2;
+        Console.WriteLine($"This is the category you entered {Category}");
     }
 }
 
@@ -37,6 +41,14 @@ publc class MobileMoneyProcessor : ItransactionProcessor
     void Process()
     {
         Console.WriteLine("This is your Mobile Money transfer");
+        Console.WriteLine("Please Enter Amount");
+        int user3 = Console.ReadLine();
+        Amount = user3;
+        Console.WriteLine($"This is the Amount you entered {Amount}");
+        Console.WriteLine("Please Enter Category");
+        string user5 = Console.ReadLine();
+        Category = user5;
+        Console.WriteLine($"This is the category you entered{Category}");
     }
 }
 
